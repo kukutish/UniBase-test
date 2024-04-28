38892ee3e7452ce7b22925e1b806026b61f53a8f
Anastasiia Manchenko <45871835+kukutish@users.noreply.github.com>
2024-04-29 00:37:54 +0500

    third task, sorting and filtering the table, changing function-createTable

---
+++ practice-3/script-3.js
@@ -0,0 +1,80 @@
+import getPosts from "../practice-2/functions/function-getPosts.js";
+import createTable from "../practice-2/functions/function-createTable.js";
+const postTable = document.getElementById('table-js');
+const postTableBody = document.getElementById('tbody-js');
+
+let posts = [];
+let postsSorted = [];
+async function displayPostsAsync() {
+  try {
+    posts = await getPosts();
+    postTable.appendChild(createTable(posts));
+    postsSorted = Array.from(posts);
+    attachEventListeners();
+  } catch (error) {
+    console.log('Ошибка при обработке постов:', error);
+    postTableBody.innerHTML = 'Ошибка при загрузке данных!';
+  }
+}
+document.addEventListener('DOMContentLoaded', ()=> displayPostsAsync())
+
+
+function sortedTable(objKey, val){
+  if(val) {
+    return postsSorted.sort((a, b) => {
+      if (typeof a[objKey] === 'number') {
+        return a[objKey] - b[objKey]
+      } else {
+        return a[objKey].localeCompare(b[objKey])
+      }
+    })
+  }
+  return postsSorted.sort((a, b) => {
+    if(typeof a[objKey] === 'number'){
+      return b[objKey] - a[objKey]
+    } else {
+      return b[objKey].localeCompare(a[objKey])
+    }
+  });
+}
+
+function attachEventListeners(){
+
+  const headers = [
+    {elem:document.getElementById('user-id-js'), key:'userId', val:false},
+    {elem:document.getElementById('id-js'), key:'id', val:false},
+    {elem:document.getElementById('title-js'), key:'title', val:false},
+    {elem:document.getElementById('body-js'), key:'body', val:false},
+  ]
+
+  headers.forEach((elem) => {
+    elem.elem.addEventListener("click", ()=>{
+      sortedTable(elem.key, !elem.val)
+      elem.val = !elem.val;
+      updateTable(postsSorted)
+    })
+  })
+}
+
+function updateTable(arr) {
+  postTable.removeChild(postTable.lastChild)
+  postTable.appendChild(createTable(arr));
+}
+
+const searchInput = document.getElementById('searchInput')
+searchInput.addEventListener('input', (evt)=>{
+  const inputText = evt.target.value.replaceAll(' ', '').toLowerCase();
+  if(inputText.length >= 3){
+    const filterPosts = postsSorted.filter(elem => {
+      const titleText = elem.title.replace(/\n/g, '').replaceAll(' ', '').toLowerCase().includes(inputText);
+      const titleBody = elem.body.replace(/\n/g, '').replaceAll(' ', '').toLowerCase().includes(inputText);
+      return (titleText || titleBody)
+    })
+    updateTable(filterPosts)
+  } else {
+    updateTable(postsSorted)
+  }
+
+})
+
+
